1774408391c206c3efb3418563023d915d447e60
KingOfMyHeart <kingofmyheart.bubu@gmail.com>
2016-02-29 17:34:37 +0000

    updated change password (rendering the form issue) show auction page hide the bid history when no one has bid on the auction

--- web/assets/js/auction_show.js
+++ web/assets/js/auction_show.js
@@ -18,7 +18,7 @@ $.ajax({
       }
       
     } else {
-      $this.html(event.strftime('</span>Time Left : %H hours : %M mins : %S seconds</span>'));
+      $this.html(event.strftime('</span>Time Left : %D days %H hours : %M mins : %S seconds</span>'));
     }
     });
   }
